3aeca0b2558f9044f649256ba7ad4afde2c94ae1
Görkem <gorkemyelken@gmail.com>
2021-05-09 13:48:15 +0300

    Homework Day 5

--- CampHomeworkDay5/e-Commerce System/src/eCommerceSystem/business/abstracts/UserService.java
+++
@@ -1,9 +0,0 @@
-package eCommerceSystem.business.abstracts;
-
-import eCommerceSystem.entities.concretes.User;
-
-public interface UserService {
-	void register(User user);
-	void registerWithGoogle(User user);
-	void login(String eMail, String password);
-}
--- CampHomeworkDay5/e-Commerce System/src/eCommerceSystem/googleAuth/GoogleAuth.java
+++
@@ -1,9 +0,0 @@
-package eCommerceSystem.googleAuth;
-
-import eCommerceSystem.entities.concretes.User;
-
-public class GoogleAuth {
-	public void register(User user) {
-		System.out.println("User registered successfully with Google account.");
-	}
-}
